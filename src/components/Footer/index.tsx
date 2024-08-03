91d2d21f0c6b5ea6529362bdb830f09dbdc1af82
suleman <sullukhan123786@gmail.com>
2024-08-03 11:24:54 +0530

    removed

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -172,7 +172,7 @@ const Footer = () => {
           <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
           <div className="py-8">
             <p className="text-center text-base text-body-color dark:text-white">
-              22 Jolimont Street, Caroline Springs, Victoria 3023, Australia
+              Caroline Springs, Victoria 3023, Australia
             </p>
           </div>
         </div>
